94045fd73ed15cf1e59eca8bd67ae6030d5c4677
mati <matee803@gmail.com>
2023-11-23 17:27:31 +0600

    cors updated and bug fixed and deployed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://hotel-room-4e4a9.web.app","https://hotel-room-4e4a9.firebaseapp.com/"],
     credentials: true,
   })
 );
